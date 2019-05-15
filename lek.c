54ae8e68b3e4974376566eaa2a73f42d4dc694d3
Mikael Blomstrand <mbloms@kth.se>
2019-05-15 13:49:14 +0200

    read em back

--- lek.c
+++ lek.c
@@ -3,26 +3,39 @@
 #include <string.h>
 
 typedef struct header {
-    void * next;
+    struct header * next;
     unsigned int size;
     void * forwarding;
 } memheader;
 
+#define MAX_HEAPSIZE (1024*1024*1024)
+
 void* heapstart;
 void* heapend;
+
+unsigned int ptrdiff(void* a, void* b) {
+    return ((char *)a) - ((char *)b);
+}
 
 
 // gc'd allocation
 void * galloc(int size){
     if (heapend == NULL) {
-        heapstart = malloc(1024*1024*1024);
-        heapend = &heapstart;
+        heapstart = malloc(MAX_HEAPSIZE);
+        heapend = heapstart;
+    }
+
+    //printf("%d\n",ptrdiff(heapend, heapstart));
+
+    if ((ptrdiff(heapend, heapstart) + sizeof(memheader) + size) > MAX_HEAPSIZE) {
+        printf("ajabaja\n");
+        return NULL;
     }
 
     memheader* header = heapend;
     void* block = heapend + sizeof(memheader);
     heapend += sizeof(memheader) + size;
-    printf("%p\n%p\n%p\n", header, block, heapend);
+    //printf("Header: %p\nBlock: %p\nHeapened: %p\n", header, block, heapend);
 
     header->next = heapend;
     header->size = size;
@@ -36,15 +49,19 @@ void collect(void *roots[]){
 }
 
 int main(int argc, char *argv[]) {
-    printf("Hello World\n");
 
-    printf("%p\n%p\n", heapend, heapstart);
     char* hej;
+    int i = 1;
+    for(char* hej = galloc(1024*8); hej!=NULL; hej = galloc(1024*8)) {
+        sprintf(hej,"hej %d", i);
+        //printf("Text: %s\nAmount of Hejs: %d\n", hej, i);
+        i++;
+    }
 
-    while(1) {
-        hej = galloc(20);
-        strcpy(hej,"hej");
-        printf("%s\n", hej);
+    for(memheader* p = heapstart;
+        ptrdiff(p,heapstart) < MAX_HEAPSIZE;
+        p=p->next) {
+        printf("%p\n%s\n\n", p, p+1);
     }
 
 
