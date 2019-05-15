effc12a63c46e0633d6c96e7487fec6f82023276
Mikael Blomstrand <mbloms@kth.se>
2019-05-15 12:00:04 +0200

    this is bad

--- lek.c
+++ lek.c
@@ -1,26 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+typedef struct header {
+    void * next;
+    void * forwarding;
+} memheader;
 
-int main(int argc, char *argv[]) {
-    printf("Hello World\n");
-    char *high = malloc(1024*1024*1024);
-    char *low = malloc(1024*1024*1024);
-    char *free = malloc(1024*1024*1024);
-    printf("Skriv något\n> ");
-    scanf("%s", high);
-    printf("%s\n",high);
-    printf("Skriv mer!\n> ");
-    scanf("%s", low);
-    printf("%s\n",low);
-    printf("MEEER!!!\n> ");
-    scanf("%s", free);
-    printf("%s\n",free);
-    scanf("%s", NULL);
-    exit(0);
-  }
+void* heap;
+static memheader* heappointer;
+
 
 // gc'd allocation
 void * galloc(int size){
-    
+    heappointer->next = heappointer + sizeof(memheader) + size;
+    printf("heappointer: %p\nnext: %p\nsizeof(memheader): %d\nsize: %d\n",heappointer, heappointer->next, sizeof(memheader), size);
+    heappointer->forwarding = NULL;
+    void* new_block = heappointer + sizeof(memheader);
+    heappointer = heappointer->next;
+    return new_block;
 }
+
+void collect(void *roots[]){
+
+}
+
+int main(int argc, char *argv[]) {
+    printf("Hello World\n");
+
+    heap = malloc(1024*1024*1024);
+    heappointer = (memheader*) &heap;
+    char* hej;
+
+    printf("%d\n", sizeof(memheader));
+
+    while(1) {
+        hej = galloc(20);
+        strcpy(hej,"hej");
+        printf("%s\n%p\n", hej,hej);
+    }
+
+
+
+    //low = malloc(1024*1024*1024);
+    //empty = malloc(1024*1024*1024);
+    //printf("Skriv något\n> ");
+    //scanf("%s", high);
+    //printf("%s\n",high);
+    exit(0);
+  }
